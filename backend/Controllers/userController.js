d0bfbc6ccacd010bca9c32936c44ec9a9b62fde7
riteshsingla36 <ritesh.singla36@gmail.com>
2022-05-03 22:51:58 +0530

    user controller updated

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,39 +1,77 @@
 const router = require("express").Router()
 const JWT = require("jsonwebtoken")
 const User = require("../Models/userModel")
+const bcrypt = require("bcrypt")
 
 
 router.post("/signup", async (req, res) => {
+    const { firstname, lastname, email, image, password } = req.body
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    if (!hashedPassword) {
+        return res.json({ status: false, msg: "Error generating hashed password, please try again" })
+    }
+
     try {
-        const user = await User.create(req.body)
+        const user = await User.create({ firstname, lastname, email, image, password: hashedPassword })
+
+
         JWT.sign({ user }, process.env.SECRET, { expiresIn: "10h" }, (err, token) => {
             if (err) {
-                res.status(400).send({ error: err.message })
+                return res.json({ status: false, msg: err.message })
             }
             else {
-                res.status(201).send({ token: token, user })
+                return res.json({ status: true, user: user, token: token })
             }
         })
     }
     catch (err) {
-        res.status(400).send({ error: err.message })
+        return res.json({ status: false, msg: err.message })
     }
 })
 
 router.post("/login", async (req, res) => {
+    const { email, password } = req.body
     try {
-        const user = await User.findOne(req.body)
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.json({ status: false, msg: "email or password is incorrect" })
+        }
+        if (user.password !== password) {
+            return res.json({ status: false, msg: "email or password is incorrect" })
+        }
+
         JWT.sign({ user }, process.env.SECRET, { expiresIn: "10h" }, (err, token) => {
             if (err) {
-                res.status(400).send({ error: err.message })
+                return res.json({ status: false, msg: err.message })
             }
             else {
-                res.status(200).send({ token: token, user })
+                return res.json({ status: true, user: user, token: token })
             }
         })
     }
     catch (err) {
-        res.status(400).send({ error: err.message })
+        return res.json({ status: false, msg: err.message })
+    }
+})
+
+router.patch("/:id", async (req, res) => {
+    const { image, bookmark } = req.query
+    try {
+        let user = await User.findById(req.params.id)
+        let bookmarks = []
+        if (bookmark) {
+            bookmarks = [...user.bookmarks, bookmark]
+            user = await User.findByIdAndUpdate(req.params.id, { bookmarks }, { new: true })
+            return res.json({ status: true, user: user })
+        }
+
+        user = await User.findByIdAndUpdate(req.params.id, { image }, { new: true })
+        return res.json({ status: true, user: user })
+    }
+    catch (err) {
+        return res.json({ status: false, msg: err.message })
     }
 })
 
