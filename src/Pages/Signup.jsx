cbb200b9f6c74d60857b13ad4e0811ce73d1eeb2
Ashish kumar Pal <ashishpal438@gmail.com>
2022-05-04 01:59:19 +0530

    Signup page completed

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,7 +1,107 @@
 import React from "react";
+import banner from "../assets/twitter-signup.png"
+import logo from "../assets/twitter-logo.png"
+import googlelogo from "../assets/Google_Icon.webp"
+import applelogo from "../assets/apple-logo.png"
+import styled from "styled-components";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <Container>
+      <div>
+        <img src={banner} alt="img" />
+      </div>
+      <div>
+        <img src={logo} alt="img" width="60px" />
+        <Title>Happening now</Title>
+        <Small>Join Twitter Today.</Small>
+        <GoogleSignup>
+          <Image src={googlelogo} alt="google" />
+          <GoogleText>Sign up with Google</GoogleText>
+        </GoogleSignup>
+        <GoogleSignup>
+          <AppleImage src={applelogo} alt="img" />
+          <AppleText>Sign up with Apple</AppleText>
+        </GoogleSignup>
+
+        <EmailSignup>
+          <EmailText>Sign up with phone or email</EmailText>
+        </EmailSignup>
+
+        <ExtraSmall>By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.</ExtraSmall>
+      </div>
+    </Container>
+  )
 };
 
 export default Signup;
+
+const Container = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
+const Title = styled.h1`
+  font-size: 4.5em;
+  text-align: left;
+`;
+
+const Small = styled(Title)`
+  font-size: 2.3em;
+`
+
+const GoogleSignup = styled.div`
+  width:50%;
+  padding: 0.4em 0.5em;
+  border: 1px solid #d1c9c9;
+  border-radius: 40px;
+  color: black;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  margin-bottom : 10px;
+  &:hover {
+    cursor:pointer;
+    background-color: #cfcfcf5f;
+  }
+`
+
+const Image = styled.img`
+  width : 25px;
+  height : 25px;
+`
+
+const GoogleText = styled.h4`
+ color : #808080;
+ font-weight: 600;
+ margin:0;
+ padding:0;
+`;
+
+const AppleImage = styled.img`
+  width :25px;
+  height : 25px;
+`;
+
+const AppleText = styled(GoogleText)`
+  color : black
+`;
+
+const EmailSignup = styled(GoogleSignup)`
+  background-color: #00a7fa;
+  padding: 0.5em;
+  &:hover{
+    background-color:#0792d8;
+  }
+`;
+
+const EmailText = styled(AppleText)`
+  color:white;
+`;
+
+const ExtraSmall = styled.p`
+  width: 50%;
+  font-size: 11px;
+`
